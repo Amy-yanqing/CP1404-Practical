eb0e8de527ec35572c79a825149ee59f10a10428
Amy zhong <yanqing.zhong@my.jcu.edu.au>
2022-09-20 17:55:00 +1000

    Add password check program

---
+++ pra_02/password_stars.py
@@ -0,0 +1,11 @@
+def main():
+    minimum_length = 4
+    password_character = input("Enter new password? ")
+    while len(password_character) <= minimum_length:
+        print("Invalid")
+        password_character = input("Enter new password? ")
+    print(password_character)
+    print(len(password_character) * "*")
+
+
+main()
