a2a046b8de0b50f3b7c09b01bfe0212aaf1954af
Amy zhong <yanqing.zhong@my.jcu.edu.au>
2022-09-20 18:01:38 +1000

    Refactor password check program to use functions

--- pra_02/password_stars.py
+++ pra_02/password_stars.py
@@ -1,11 +1,20 @@
 def main():
     minimum_length = 4
+    password_character = get_password(minimum_length)
+    print_stars_line(password_character)
+
+
+def print_stars_line(password_character):
+    print(len(password_character) * "*")
+
+
+def get_password(minimum_length):
     password_character = input("Enter new password? ")
     while len(password_character) <= minimum_length:
         print("Invalid")
         password_character = input("Enter new password? ")
     print(password_character)
-    print(len(password_character) * "*")
+    return password_character
 
 
 main()
