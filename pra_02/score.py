2666b1d2b16ef617dd448557cb21a387bd5c2c9e
Amy zhong <yanqing.zhong@my.jcu.edu.au>
2022-09-25 14:44:50 +1000

    Factor score program to use function

--- pra_02/score.py
+++ pra_02/score.py
@@ -2,30 +2,32 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
-# TODO: Fix this!
 
-# score = float(input("Enter score: "))
-# if score < 0:
-#     print("Invalid score")
-# else:
-#     if score > 100:
-#         print("Invalid score")
-#     if score > 50:
-#         print("Passable")
-#     if score > 90:
-#     print("Excellent")
-# if score < 50:
-#     print("Bad")
-#
+def main():
+    score = get_valid_score()
+    print_result(score)
+    random_score = random.randint(1, 100)
+    print("The random score is:", random_score)
+    print_result(random_score)
 
-score = float(input("Enter score: "))
-while score < 0 or score > 100:
-    print("Invalid score")
+
+def print_result(score):
+    if score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+
+
+def get_valid_score():
     score = float(input("Enter score: "))
-if score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+    return score
+
+
+main()
