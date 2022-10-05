ea69025682126bf7d4c49179355a4d7e4c84d669
Amy zhong <yanqing.zhong@my.jcu.edu.au>
2022-10-05 16:51:00 +1000

    Add if elif pattern to count each kind of character

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -1,11 +1,11 @@
 """
-CP1404/CP5632 - Practical
-Password checker "skeleton" code to help you get started
+CP1404/CP5632 - Practical - Suggested Solution
+Password checker built from "skeleton" code to help you get started
 """
 
-MIN_LENGTH = 2
-MAX_LENGTH = 6
-SPECIAL_CHARS_REQUIRED = False
+MIN_LENGTH = 5
+MAX_LENGTH = 15
+SPECIAL_CHARS_REQUIRED = True
 SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
 
 
@@ -23,13 +23,15 @@ def main():
     while not is_valid_password(password):
         print("Invalid password!")
         password = input("> ")
-    print("Your {}-character password is valid: {}".format(len(password),
-                                                           password))
+    print("Your " + str(
+        len(password)) + " character password is valid: " + password)
 
 
 def is_valid_password(password):
     """Determine if the provided password is valid."""
     # TODO: if length is wrong, return False
+    if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
+        return False
 
     count_lower = 0
     count_upper = 0
@@ -37,13 +39,21 @@ def is_valid_password(password):
     count_special = 0
     for char in password:
         # TODO: count each kind of character (use str methods like isdigit)
-        pass
+        if char.isdigit():
+            count_digit += 1
+        elif char.islower():
+            count_lower += 1
+        elif char.isupper():
+            count_upper += 1
+        elif char in SPECIAL_CHARACTERS:
+            count_special += 1
 
     # TODO: if any of the 'normal' counts are zero, return False
-
     # TODO: if special characters are required, then check the count of those
     # and return False if it's zero
 
+    if count_lower == 0 or count_upper == 0 or count_digit == 0 or count_special == 0:
+        return False
     # if we get here (without returning False), then the password must be valid
     return True
 
