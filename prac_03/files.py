df0bf19c70a915cc5ddfd2e04f062bd182d67c4d
Amy zhong <yanqing.zhong@my.jcu.edu.au>
2022-10-05 12:27:04 +1000

    Add files programming and Questions

--- prac_03/files.py
+++ prac_03/files.py
@@ -8,7 +8,7 @@ Create a text file called numbers.txt and save it in your prac directory. Put th
 42
 400
 Write code that opens "numbers.txt", reads only the first two numbers and adds them together then prints the result, which should be... 59.
-
+*****
 Now write a fourth block of code that prints the total for all lines in numbers.txt or a file with any number of numbers.
 """
 # Programming 1
