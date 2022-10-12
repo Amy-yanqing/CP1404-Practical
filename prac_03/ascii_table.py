28212ec7c86b1d66ebc2bde63febad89786da364
Amy zhong <yanqing.zhong@my.jcu.edu.au>
2022-10-12 18:05:11 +1000

    Add ascii table programming

---
+++ prac_03/ascii_table.py
@@ -0,0 +1,9 @@
+LOW_NUMBER = 33
+UPPER_NUMBER = 127
+# character = input("Enter a character: ")
+# print(ord(character))
+for number in range(LOW_NUMBER,UPPER_NUMBER):
+# number = int(input(f"Enter number between {LOW_NUMBER} to {UPPER_NUMBER}: "))
+    while number < LOW_NUMBER or number > UPPER_NUMBER:
+        number = int(input("Enter number: "))
+    print(f"{number:<4}{chr(number)}")
