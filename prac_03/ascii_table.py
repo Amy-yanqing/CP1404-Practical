02f60ee3dfe44cf93f5c934f84a3c4e73684e153
Amy zhong <yanqing.zhong@my.jcu.edu.au>
2022-10-12 18:13:22 +1000

    Modify ascii table programming and add module docstring

--- prac_03/ascii_table.py
+++ prac_03/ascii_table.py
@@ -1,9 +1,11 @@
+
+"""Get ASCII code then display the ASCII code's corresponding  characters"""
 LOW_NUMBER = 33
 UPPER_NUMBER = 127
 # character = input("Enter a character: ")
 # print(ord(character))
-for number in range(LOW_NUMBER,UPPER_NUMBER):
-# number = int(input(f"Enter number between {LOW_NUMBER} to {UPPER_NUMBER}: "))
+for number in range(LOW_NUMBER, UPPER_NUMBER):
+    # number = int(input(f"Enter number between {LOW_NUMBER} to {UPPER_NUMBER}: "))
     while number < LOW_NUMBER or number > UPPER_NUMBER:
         number = int(input("Enter number: "))
     print(f"{number:<4}{chr(number)}")
