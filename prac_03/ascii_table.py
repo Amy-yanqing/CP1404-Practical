53451feb439eded41be5bd38f4242fae18dc7ddf
Amy zhong <yanqing.zhong@my.jcu.edu.au>
2022-10-12 18:32:23 +1000

    Add for loop to generate ASCII code

--- prac_03/ascii_table.py
+++ prac_03/ascii_table.py
@@ -4,8 +4,10 @@ LOW_NUMBER = 33
 UPPER_NUMBER = 127
 # character = input("Enter a character: ")
 # print(ord(character))
-for number in range(LOW_NUMBER, UPPER_NUMBER):
-    # number = int(input(f"Enter number between {LOW_NUMBER} to {UPPER_NUMBER}: "))
-    while number < LOW_NUMBER or number > UPPER_NUMBER:
-        number = int(input("Enter number: "))
-    print(f"{number:<4}{chr(number)}")
+number = int(input(f"Enter number between {LOW_NUMBER} to {UPPER_NUMBER}: "))
+while number < LOW_NUMBER or number > UPPER_NUMBER:
+    number = int(input("Enter number: "))
+print(f"{number:<4}{chr(number)}")
+
+for i in range(LOW_NUMBER, UPPER_NUMBER + 1):
+    print(f"{i:3} {chr(i):>3}")
