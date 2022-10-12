f0e7307e128937b6a8aaa40b218504ff72c90cbe
Amy zhong <yanqing.zhong@my.jcu.edu.au>
2022-10-12 17:34:13 +1000

    Modify docstring

--- prac_03/practical 3 extension work.py
+++ prac_03/practical 3 extension work.py
@@ -1,9 +1,14 @@
-import random  # version 1
+# """
+# generate a random Boolean True or False with 3 different way
+# """
+# import random  # version 1
+#
+# print(bool(random.randint(0, 1)))
+#
+# random_number = bool(random.choice([True, False]))  # version 2
+# print(random_number)
+#
+# random_number = bool(random.getrandbits(1))  # version 3
+# print(random_number)
 
-print(bool(random.randint(0, 1)))
 
-random_number = bool(random.choice([True, False]))  # version 2
-print(random_number)
-
-random_number = bool(random.getrandbits(1))  # version 3
-print(random_number)
