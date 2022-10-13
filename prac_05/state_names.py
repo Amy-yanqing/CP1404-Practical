650ceb158def85dafcf21a08b777dbba43fd40f3
Amy zhong <yanqing.zhong@my.jcu.edu.au>
2022-10-13 16:50:22 +1000

    Modify state names program add exception approach

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -7,12 +7,18 @@ File needs reformatting
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ")
+state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
+    try:
         print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
-        print("Invalid short state")
-    state_code = input("Enter short state: ")
+    except KeyError:
+        print("Invalid short state KeyError")
+    state_code = input("Enter short state: ").upper()
+try:
+    print(f"{state_code}:{CODE_TO_NAME[state_code]}")
+except:
+    print("Empty choice")
+
+for state in CODE_TO_NAME:
+    print(f"{state}:{CODE_TO_NAME[state]}")
