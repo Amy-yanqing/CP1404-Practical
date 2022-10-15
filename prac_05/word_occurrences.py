9795da680d0ea7a24be1b496eedb16bccc576f4f
Amy zhong <yanqing.zhong@my.jcu.edu.au>
2022-10-15 18:39:01 +1000

    Modify get max word length method

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -6,10 +6,10 @@ Word Occurrences
 Estimate: 30 minutes
 Actual:   60  minutes
 """
+word_to_count = {}
 TEXT = "this is a collection of words of nice words this is a fun thing it is"
 # Text = input("enter your text： ")
 words = TEXT.split()
-word_to_count = {}
 for word in words:  # version 1
     try:
         word_to_count[word] += 1
@@ -17,6 +17,6 @@ for word in words:  # version 1
         word_to_count[word] = 1
 # for word in words:  # version 2
 #     word_to_count[word] = word_to_count.get(word, 0) + 1
-word_length = max(len(word) for word in list(word_to_count.keys()))
+word_length = max(len(word) for word in words)
 for word, count in sorted(word_to_count.items()):
     print(f"{word:{word_length}} :{count}")
