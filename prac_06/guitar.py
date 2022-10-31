debdd4db57114d36cf8165c353c96394f0f55116
Amy zhong <yanqing.zhong@my.jcu.edu.au>
2022-10-31 12:24:44 +1000

    Add guitar class program

---
+++ prac_06/guitar.py
@@ -0,0 +1,15 @@
+
+class Guitar:
+    """Represent a Car object.."""
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return a string representation of a guitar object."""
+        return f"{self.name}({self.year}) : ${self.cost:.2f}"
+
+
+my_guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
+print(my_guitar)
