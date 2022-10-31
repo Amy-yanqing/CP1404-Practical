2dab6927521cd732cba9b0516743535605bb0e3b
Amy zhong <yanqing.zhong@my.jcu.edu.au>
2022-10-31 20:35:11 +1000

    Fixed print descriptions

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,13 +1,20 @@
+"""
+a program tests for Guitar class
+"""
+
 from prac_06.guitar import Guitar
 
 
-def main():
+def run_tests():
+    """test for Guitar class"""
+
     my_guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
     another_guitar = Guitar("Another Guitar", 2013, )
-    print(f"Expected 100. Got {my_guitar.get_age()}")
-    print(f"Expected 9. Got {another_guitar.get_age()}")
-    print(f"Expected True. Got {my_guitar.is_vintage()}")
-    print(f"Expected False. Got {another_guitar.is_vintage()}")
+
+    print(f"{my_guitar.name} get_age() - Expected 100. Got {my_guitar.get_age()}")
+    print(f"{another_guitar.name} get_age() - Expected 9. Got {another_guitar.get_age()}")
+    print(f"{my_guitar.name} is_vintage() - Expected True. Got {my_guitar.is_vintage()}")
+    print(f"{another_guitar.name} is_vintage() - Expected False. Got {another_guitar.is_vintage()}")
 
 
-main()
+run_tests()
