8cdfb27784c6b5cf5907d1138696217b90c3caca
Amy zhong <yanqing.zhong@my.jcu.edu.au>
2022-10-31 19:49:47 +1000

    Add function docstrings

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -18,12 +18,15 @@ class Guitar:
         return f"{self.name}({self.year}) : ${self.cost:.2f}"
 
     def get_age(self):
+        """get age method"""
         current_year = 2022
         return current_year - self.year
 
     def is_vintage(self):
+        """determine it is old guitar"""
         return self.get_age() >= 50
 
 
-my_guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
-print(my_guitar)
+if __name__ == '__main__':
+    my_guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    print(my_guitar)
