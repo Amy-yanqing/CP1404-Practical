fc83b5a19d4deef075498033b5df17edd1ed751b
Amy zhong <yanqing.zhong@my.jcu.edu.au>
2022-10-31 13:08:32 +1000

    Add get_age and is_vintage method

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -3,8 +3,11 @@ Estimate: 30 minutes
 Actual: 60 minutes
 Programming guitar class
 """
+
+
 class Guitar:
     """Represent a Car object.."""
+
     def __init__(self, name="", year=0, cost=0):
         self.name = name
         self.year = year
@@ -13,6 +16,13 @@ class Guitar:
     def __str__(self):
         """Return a string representation of a guitar object."""
         return f"{self.name}({self.year}) : ${self.cost:.2f}"
+
+    def get_age(self):
+        current_year = 2022
+        return current_year - self.year
+
+    def is_vintage(self):
+        return self.get_age() >= 50
 
 
 my_guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
