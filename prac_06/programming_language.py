87e422b5d4b967c35b28d2b07ef82b259e7e1c0e
Amy zhong <yanqing.zhong@my.jcu.edu.au>
2022-10-31 12:04:24 +1000

    Fixed list

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -24,10 +24,10 @@ class ProgrammingLanguage:
 
 def run_tests():
     """test dynamically language typed """
-    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-    languages = [ruby, python, visual_basic]
+    languages = [python, ruby, visual_basic]
     print("The dynamically typed languages are:")
     for language in languages:
         if language.is_dynamic():
