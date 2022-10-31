0411ae0e8899797c1ec1834d200d2b8d3228db4b
Amy zhong <yanqing.zhong@my.jcu.edu.au>
2022-10-31 12:09:27 +1000

    remove run test method

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -20,19 +20,3 @@ class ProgrammingLanguage:
     def __str__(self):
         """Return string representation of a ProgrammingLanguage."""
         return f"{self.field},{self.typing} Typing,Reflection={self.reflection},First appeared in {self.year}"
-
-
-def run_tests():
-    """test dynamically language typed """
-    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-    languages = [python, ruby, visual_basic]
-    print("The dynamically typed languages are:")
-    for language in languages:
-        if language.is_dynamic():
-            print(language.field)
-
-
-if __name__ == "__main__":
-    run_tests()
