dbfb4db6291deedbba586cd070b93e0b88403df5
Amy zhong <yanqing.zhong@my.jcu.edu.au>
2022-10-31 12:10:58 +1000

    Add main function to check dynamically typed languages

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,10 +1,22 @@
 """start:11:15"""
 
+"""
+Programming Language client code.
+"""
+
 from prac_06.programming_language import ProgrammingLanguage
 
-python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 
-print(python)
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 
+    languages = [python, ruby, visual_basic]
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.field)
+
+
+main()
