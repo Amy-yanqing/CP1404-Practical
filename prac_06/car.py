d8f8b06b5719f5575eb1dcbaa3b31dbd485e2ddc
Amy zhong <yanqing.zhong@my.jcu.edu.au>
2022-10-31 11:22:10 +1000

    Add docstring for str method

--- prac_06/car.py
+++ prac_06/car.py
@@ -29,4 +29,5 @@ class Car:
         return distance
 
     def __str__(self):
+        """Return a string representation of a Car object."""
         return f"{self.name},fuel={self.fuel},odometer={self._odometer}"
