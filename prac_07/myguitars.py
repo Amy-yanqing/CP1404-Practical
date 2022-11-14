f9e5b1b7d39d7e4d9930937b72fb9f2f812ec58d
Amy zhong <yanqing.zhong@my.jcu.edu.au>
2022-11-14 19:00:03 +1000

    Add docstring for my guitar program

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,3 +1,8 @@
+"""
+Estimate: 60 minutes
+Actual: 120 minutes
+Programming guitar
+"""
 import csv
 from prac_07.guitar import Guitar
 
@@ -5,6 +10,7 @@ file_name = 'guitars.csv'
 
 
 def main():
+    """A program about modify guitar data"""
     guitars = []
     choice = input("L(oad),D(display),A(dd),S(ave),Q(uit)?  ").upper()
     while choice != "Q":
@@ -24,12 +30,14 @@ def main():
 
 
 def save_guitar(guitars):
+    """Save guitar data to file"""
     with open(file_name, 'w') as out_file:
         for guitar in guitars:
             print(guitar.name, ",", guitar.year, ",", guitar.cost, file=out_file)
 
 
 def add_guitar(guitars):
+    """Add new guitar to file"""
     name = input("Guitar name: ")
     while name != "":
         year = int(input("year: "))
@@ -40,6 +48,7 @@ def add_guitar(guitars):
 
 
 def load_guitar_file():
+    """Load guitar data"""
     guitars = []
     with open(file_name, 'r') as in_file:
         for line in in_file:
@@ -49,6 +58,7 @@ def load_guitar_file():
 
 
 def display_guitar(guitars):
+    """Display guitar in the guitars"""
     for guitar in guitars:
         print(guitar)
 
