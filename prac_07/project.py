1c40b88f149faf7c496c5ef3aac0417d8317c483
Amy zhong <yanqing.zhong@my.jcu.edu.au>
2022-11-14 19:24:08 +1000

    Add project class program

--- prac_07/project.py
+++ prac_07/project.py
@@ -7,7 +7,8 @@ class Project:
         self.completion_percentage = completion_percentage
 
     def __str__(self):
-        return f"{self.name}{self.start_date}{self.priority}{self.cost_estimate}{self.completion_percentage}"
+        return f"{self.name},start: {self.start_date},priority {self.priority}" \
+               f",estimate:{self.cost_estimate},completion:{self.completion_percentage}"
 
     def is_completed(self):
         return self.completion_percentage == 100
