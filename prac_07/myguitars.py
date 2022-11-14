08a4d74365210f762f9acad6296f6c6a078098fd
Amy zhong <yanqing.zhong@my.jcu.edu.au>
2022-11-14 18:55:46 +1000

    Complete second part Guitars exercise add function

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -6,12 +6,51 @@ file_name = 'guitars.csv'
 
 def main():
     guitars = []
+    choice = input("L(oad),D(display),A(dd),S(ave),Q(uit)?  ").upper()
+    while choice != "Q":
+        if choice == "L":
+            guitars = load_guitar_file()
+            print(f"Guitar {len(guitars)} file load")
+        elif choice == "D":
+            display_guitar(guitars)
+        elif choice == "A":
+            add_guitar(guitars)
+        elif choice == "S":
+            save_guitar(guitars)
+        else:
+            print("Invalid Choices")
+        choice = input("L(oad),D(display),A(dd),S(ave),Q(uit)?  ").upper()
+    print("Bye")
+
+
+def save_guitar(guitars):
+    with open(file_name, 'w') as out_file:
+        for guitar in guitars:
+            print(guitar.name, ",", guitar.year, ",", guitar.cost, file=out_file)
+
+
+def add_guitar(guitars):
+    name = input("Guitar name: ")
+    while name != "":
+        year = int(input("year: "))
+        cost = float(input("Cost: "))
+        guitar_to_add = Guitar(name, year, cost)
+        guitars.append(guitar_to_add)
+        name = input("Name: ")
+
+
+def load_guitar_file():
+    guitars = []
     with open(file_name, 'r') as in_file:
         for line in in_file:
             parts = line.strip().split(',')
-            guitars.append(Guitar(*parts))
+            guitars.append(Guitar(parts[0], parts[1], parts[2]))
+    return guitars
 
-    guitars.sort()
+
+def display_guitar(guitars):
     for guitar in guitars:
         print(guitar)
+
+
 main()
